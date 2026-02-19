3ca116298c04cd4abd05d7255d8cb40f25a5f634
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-02-19 13:46:47 +0530

    stuck with string, array knowledge. apart from template functions, full fledged function needs to be learnt

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repetition) {
+    
+    let str = '';
 
+    if (repetition < 0){
+        return "ERROR";
+    }
+
+    for (let i = 0; i<repetition; i++){
+        str = str + string;
+    }
+   
+    return str;
 };
 
 // Do not edit below this line
