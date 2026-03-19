abde599b722d2771965a2a4cf1b949e2bf447a69
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-03-19 22:07:13 +0530

    using typeof to work with type errors. multiply and divide by 10 to check for decimals since typeof is limited by umbrella term numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,35 @@
 const sumAll = function() {
 
+    if (arguments.length != 2){
+        return 'ERROR';
+    }
+
+    if (arguments[0] == arguments[1]){
+        return arguments[0];
+    }
+
+    if (arguments[0] < 0 || arguments[1] < 0){
+        return 'ERROR';
+    }
+
+    if (typeof(arguments[0]) != typeof(1) || typeof(arguments[1]) != typeof(1))
+    {
+        return 'ERROR';    
+    }
+
+    if ((arguments[0] * 10)%10 != 0 || (arguments[1] * 10)%10 != 0){
+        return 'ERROR';
+    }
+
+    let start = (arguments[0] < arguments[1])? arguments[0] : arguments[1];
+    let end = (arguments[0] > arguments[1])? arguments[0] : arguments[1];
+    let sum = start+end;
+
+    while (++start < end){
+        sum+=start;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
