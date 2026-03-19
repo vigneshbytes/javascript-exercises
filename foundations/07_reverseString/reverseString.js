446283cf693faa82c9810bbe39c52187360ff673
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-03-19 18:23:49 +0530

    resuming js after so long. almost finished Matt Frisbe book, feeling confident about js now

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (string.length <= 0){
+        return "";
+    }
 
+    let arr = new Array(string.length);
+    for (let i=0; i<string.length; i++){
+        arr[i] = string.charAt((string.length) - (i+1));
+    }
+
+    return arr.join("");
 };
 
 // Do not edit below this line
