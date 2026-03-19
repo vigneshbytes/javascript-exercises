26cf3e1b1ba1d317bf146cd4cd4fc1c70fad4766
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-03-19 19:55:09 +0530

    gotta practice to improve memory and get familiar with methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
 const removeFromArray = function() {
+
+    if (arguments.length <2) {
+        return null;
+    }
+
+    let argLen = arguments.length;
+    let arr = arguments[0].slice(); //imp to clone, or else accessed by reference? which may modify the input arg?
+
+    for (let i = 1; i< argLen; i++){
+        targetVal = arguments[i];
+        
+        let tgtIndex = arr.indexOf(targetVal);
+
+        while (tgtIndex != -1) {        
+            
+            arr.splice(tgtIndex, 1);
+
+            // tries to search again, for multiple instances of tgt value            
+            tgtIndex = arr.indexOf(targetVal); 
+        }
+    }
+
+
+    return arr;
 };
 
 // Do not edit below this line
